2dad45dbefd2d084b455df48e316df06a6d6171e
alexsaushkin <thirdyou@yandex.ru>
2023-07-14 13:47:30 +0300

    fix: fix cookies deleting

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,6 +49,7 @@ module.exports.signin = (req, res, next) => {
         sameSite: 'none',
         secure: true,
         maxAge: 3600000 * 24 * 7,
+        path: '/',
       });
       res.send({ message: 'Успешный вход' });
     })
